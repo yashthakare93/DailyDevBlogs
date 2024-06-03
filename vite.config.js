23dd025f81382be7512e4de16e0fcef88cb1252d
Yash Thakare <thakareyash74@gmail.com>
2024-06-03 20:13:16 +0530

    Server issue fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server:{
+    watch:{
+      usePolling:true
+    }  
+  },
   base: '/',
   resolve: {
     alias: {
